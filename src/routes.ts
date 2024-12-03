b129fb997dc5cb7b55204b8762c4ebdb90012df9
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2024-12-03 23:05:24 +0300

    try gh pages 6

--- src/routes.ts
+++ src/routes.ts
@@ -1,35 +1,37 @@
 import { createRouterConfig } from 'dk-react-mobx-router';
 
+import { env } from 'env';
+
 export const routes = createRouterConfig({
   install: {
-    path: '/',
+    path: `${env.ASSETS_PREFIX}/`,
     loader: () => import('pages/install') as any,
     params: {},
   },
   textInput: {
-    path: '/text-input',
+    path: `${env.ASSETS_PREFIX}/text-input`,
     loader: () => import('pages/textInput') as any,
     params: {},
   },
   submit: {
-    path: '/submit',
+    path: `${env.ASSETS_PREFIX}/submit`,
     loader: () => import('pages/submit') as any,
     params: {},
   },
   formConfigure: {
-    path: '/form/configure',
+    path: `${env.ASSETS_PREFIX}/form/configure`,
     loader: () => import('pages/formConfigure') as any,
     params: {},
   },
 
   error404: {
-    path: '/error404',
+    path: `${env.ASSETS_PREFIX}/error404`,
     loader: () => import('pages/error') as any,
     props: { errorNumber: 404 },
     params: {},
   },
   error500: {
-    path: '/error500',
+    path: `${env.ASSETS_PREFIX}/error500`,
     loader: () => import('pages/error') as any,
     props: { errorNumber: 500 },
     params: {},
